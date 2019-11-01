0e2a3cb0d1604b1c1384579c32396821bb3889c3
veenhof <35380772+veenhof@users.noreply.github.com>
2019-11-01 15:02:38 +0100

    Update Start_PIR_ETC.py

--- Start_PIR_ETC.py
+++ Start_PIR_ETC.py
@@ -6,19 +6,19 @@ import string     # helps parse strings
 import os
 
 switch1 = 24 # enter your switch gpio number here     PIR
-switch2 = 20 # enter your switch gpio number here     TOUCH
+#switch2 = 20 # enter your switch gpio number here     TOUCH
 
 relay1 = 17 # enter your relay gpio number here    SCREEN
 
-state1 = 0
-state2 = 0
+#state1 = 0
+#state2 = 0
 
 
 GPIO.setmode(GPIO.BCM) # you can change this to Board if you prefere
 GPIO.setwarnings(False)
 
 GPIO.setup(switch1, GPIO.IN)
-GPIO.setup(switch2, GPIO.IN)
+#GPIO.setup(switch2, GPIO.IN)
 GPIO.setup(relay1, GPIO.OUT)
 
 # set relay off you may need to change this to high if you relay board works active on low, also change state to 1 if this is the case.
@@ -27,19 +27,22 @@ GPIO.output(relay1, GPIO.LOW)
 while True:
 
 # check if switch is pressed and keep checking
-    while GPIO.input(switch1) == 1 and GPIO.input(switch2) == 1:
-        print "Touch sensor ON..."
-        time.sleep(5)
+    while GPIO.input(switch1) == 1:     # and GPIO.input(switch2) == 1:
+        print "PIR sensor ON..."
+        time.sleep(2)
         print "Sensor activated..."
-
+        GPIO.output(relay1, GPIO.HIGH)
+        time.sleep(10)
+            
+    
+    
     else:
 # checks current relay state and changes it to the other.
-        if GPIO.input(switch2) == 0 and GPIO.input(switch1) == 1:
+        if GPIO.input(switch1) == 0:            # and GPIO.input(switch1) == 1:
             print "Movement detected! " + (time.strftime("%H:%M:%S"))
-            state1 = 1
-            GPIO.output(relay3, GPIO.HIGH)
+            GPIO.output(relay1, GPIO.LOW)
             #DOE JE DINGETJE
-            requests.put('http://192.168.2.175:8080/rest/items/PIR/state', str(ON))
+            #requests.put('http://192.168.2.175:8080/rest/items/PIR/state', str(ON))
             time.sleep(2)
             state1 = 0
 
