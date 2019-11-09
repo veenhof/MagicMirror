2ac3d64fca0ff5fce8b9e5af80cf5ab501a8a748
veenhof <35380772+veenhof@users.noreply.github.com>
2019-11-09 02:45:42 +0100

    Update Start_PIR_ETC.py

--- Start_PIR_ETC.py
+++ Start_PIR_ETC.py
@@ -10,8 +10,8 @@ switch1 = 24 # enter your switch gpio number here     PIR
 
 relay1 = 17 # enter your relay gpio number here    SCREEN
 
-#state1 = 0
-#state2 = 0
+state1 = 0
+state2 = 0
 
 
 GPIO.setmode(GPIO.BCM) # you can change this to Board if you prefere
@@ -26,51 +26,28 @@ GPIO.output(relay1, GPIO.LOW)
 
 while True:
 
+    
+#IF ON, then relay ( scherm ) on    
 # check if switch is pressed and keep checking
     while GPIO.input(switch1) == 1:     # and GPIO.input(switch2) == 1:
         print "PIR sensor ON..."
         time.sleep(2)
         print "Sensor activated..."
-        GPIO.output(relay1, GPIO.HIGH)
-        time.sleep(10)
+        GPIO.output(relay1, GPIO.HIGH)    #Maybe need to change this to LOW    because of the GIO.Output
+        state1 = 1
+        time.sleep(100)
             
     
     
     else:
 # checks current relay state and changes it to the other.
+# else pir is low ( no movement ), scherm uit
         if GPIO.input(switch1) == 0:            # and GPIO.input(switch1) == 1:
-            print "Movement detected! " + (time.strftime("%H:%M:%S"))
-            GPIO.output(relay1, GPIO.LOW)
+            #print "Movement detected! " + (time.strftime("%H:%M:%S"))
+            GPIO.output(relay1, GPIO.LOW)     #Maybe need to change this to LOW    because of the GIO.Output
             #DOE JE DINGETJE
             #requests.put('http://192.168.2.175:8080/rest/items/PIR/state', str(ON))
             time.sleep(2)
             state1 = 0
-
-
-
-        elif GPIO.input(switch1) == 1 and GPIO.input(switch2) == 0:
-            #elif state2 == 0:
-            print "Setting SWITCH 2  gpio high"
-            requests.put('http://192.168.2.175:8080/rest/items/Touch/state', str(average))
-            state2 = 1
-            GPIO.output(relay1, GPIO.HIGH)
-            time.sleep(3)
-            GPIO.output(relay1, GPIO.LOW)
-            time.sleep(3)
-            GPIO.output(relay2, GPIO.LOW)
-            state2 = 0
-            time.sleep(1)
-        else:
-            print "setting gpio low"
-            state1 = 0
-            state2 = 0
-            GPIO.output(relay1, GPIO.LOW)
-            time.sleep(1)
-
-# check for switch released and keep checking
-    while GPIO.input(switch1) == 0 or GPIO.input(switch2) == 0:
-        print "waiting for switch release"
-        time.sleep(0.5)
-
 
 
